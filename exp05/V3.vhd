0aa0fdeefad7ebfe7769452b77daf775c78fa6fa
marcosblandim <marcosblandim@gmail.com>
2019-10-16 16:53:30 -0300

    Exp 5 terminado

--- exp05/V3.vhd
+++ exp05/V3.vhd
@@ -4,23 +4,25 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_arith.all;
 
 entity top_module is
-end top_module
+end top_module;
 
 architecture top_module_arch of top_module is 
-    component somador_dut is
-        port(A, B: in std_logic_vector (3 downto 0);
-            S: out std_logic_vector (4 downto 0));
+
+    component somador4 is
+        port(A, B: in std_logic_vector(3 downto 0);
+            S: out std_logic_vector(4 downto 0));
     end component;
+
+    component test_bench is
+        port(s_dut, s_gm: in std_logic_vector (4 downto 0);
+            A, B: out std_logic_vector (3 downto 0));
+    end component;    
 
     component somador_golden is
         port(A, B: in std_logic_vector (3 downto 0);
             S: out std_logic_vector (4 downto 0));
     end component;
-
-    component somador_dut is
-        port(s_dut, s_gm: in std_logic_vector (4 downto 0);
-        A, B: out std_logic_vector (3 downto 0));
-    end component;
+    
 
     signal A, B: std_logic_vector(3 downto 0);
     signal s_dut, s_gm: std_logic_vector(4 downto 0));
