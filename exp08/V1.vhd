30e04a918ec9068e7e3f465552c86b1a8c3e67d4
pedroblandim <pedro.blandim@gmail.com>
2019-11-06 15:20:55 -0300

    exp8 v1

--- exp08/V1.vhd
+++ exp08/V1.vhd
@@ -1,22 +1,43 @@
 library IEEE;
-use IEEE.std_logic_1164.all;
+use IEEE.STD_LOGIC_1164.ALL;
 
 entity exp8visto1 is
-    port( clock : in std_logic;
-          reset : in std_logic;
-          enable : in std_logic;
-          load : in std_logic;
-          dezload : in std_logic_vector(3 downto 0);
-          uniload : in std_logic_vector(3 downto 0);
-          dezena : out std_logic_vector(3 downto 0);
-          unidade : out std_logic_vector(3 downto 0));
+  port( clock : in STD_LOGIC;
+        reset : in STD_LOGIC;        
+        enable : in STD_LOGIC;
+        load : in STD_LOGIC;
+        dezload : in STD_LOGIC_VECTOR(3 downto 0);
+        uniload : in STD_LOGIC_VECTOR(3 downto 0);
+        dezena : out STD_LOGIC_VECTOR(3 downto 0);
+        unidade : out STD_LOGIC_VECTOR(3 downto 0) );
 end exp8visto1;
 
 architecture exp8visto1_arch of exp8visto1 is
-    --inserir sinais e componentes aqui
-begin 
+component cont10 is
+    Port ( clock : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           enable : in  STD_LOGIC;
+           rci : in  STD_LOGIC;
+           load : in  STD_LOGIC;
+           D : in  STD_LOGIC_VECTOR (3 downto 0);
+           rco : out  STD_LOGIC;
+           Q : out  STD_LOGIC_VECTOR (3 downto 0));
+end component; 
 
-    --inserir implemnetação aqui
+component divclock is
+  port( clock50MHz : in STD_LOGIC;
+        clock1Hz : out STD_LOGIC );
+end component;
+
+signal rcs, clock1, z: STD_LOGIC;
+
+begin
+
+U1: divclock port map (clock, clock1);
+
+U2: cont10 port map (clock1, reset, enable, '0', load, uniload, rcs, unidade);
+
+U3: cont10 port map (clock1, reset, enable, rcs, load, dezload, z ,dezena);
+
 
 end exp8visto1_arch;
-
--- exp08/cont10.vhd
+++ exp08/cont10.vhd
@@ -1,123 +1,145 @@
-library ieee;
-use ieee.std_logic_1164;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 
-entity contador10 is 
-    port( clock : in std_logic;
-          reset : in std_logic;
-          enable : in std_logic;
-          RCI : in std_logic;
-          load : in std_logic;
-          D : out std_logic_vector (3 downto 0);
-          Q : out std_logic_vector (3 downto 0);
-          RCO : out std_logic);
-end contador10;
+entity cont10 is
+    Port ( clock : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           enable : in  STD_LOGIC;
+           rci : in  STD_LOGIC;
+           load : in  STD_LOGIC;
+           D : in  STD_LOGIC_VECTOR (3 downto 0);
+           rco : out  STD_LOGIC;
+           Q : out  STD_LOGIC_VECTOR (3 downto 0));
+end cont10;
 
-architecture arch of contador10 is
-    type estado is (ST0, ST1, ST2, ST3, ST4, ST5, ST6, ST7, ST8, ST9);
-    signal currentState, nextState, loadState : estado;
+architecture cont10_arch of cont10 is
+	type estado is (ST0, ST1, ST2, ST3, ST4, ST5, ST6, ST7, ST8, ST9);
+	signal currentState, nextState, loadState : estado;
 begin
-    with D select
-        loadState <= ST0 when "0000",
-                  <= ST1 when "0001"
-                  <= ST2 when "0010"
-                  <= ST3 when "0011"
-                  <= ST4 when "0100"
-                  <= ST5 when "0101"
-                  <= ST6 when "0110"
-                  <= ST7 when "0111"
-                  <= ST8 when "1000"
-                  <= ST9 when "1001"
-                  <= ST0 when others;
-sync_proc : process(clock)
-begin 
-    if rising_edge(clock) then 
-        currentState <= nextState;
-    end if;
+with D select
+	loadstate <= ST0 when "0000",
+			ST1 when "0001",
+			ST2 when "0010",
+			ST3 when "0011",
+			ST4 when "0100",
+			ST5 when "0101",
+			ST6 when "0110",
+			ST7 when "0111",
+			ST8 when "1000",
+			ST9 when "1001",
+			ST0 when others;
+sync_proc: process (clock)
+begin
+	if rising_edge(clock) then
+		currentState <= nextState;
+	end if;
 end process sync_proc;
 
-comb_proc : process(currentState, reset, enable, RCI, load, loadState)
-begin 
-    case currentState is
-        when ST0 => 
-            Q <= "0000"
-            RCO <= '1'
-            if (reset = '1') then nextState <= ST0;
-            elsif (load = '1') then nextState <= loadState;
-            elsif ((enable = '0') and (RCI = '0')) then nextState <= ST1;
-            else nextState <= ST0;
-            end if;
-        when ST1 => 
-            Q <= "0001"
-            RCO <= '1'
-            if (reset = '1') then nextState <= ST0;
-            elsif (load = '1') then nextState <= loadState;
-            elsif ((enable = '0') and (RCI = '0')) then nextState <= ST2;
-            else nextState <= ST1;
-            end if;
-        when ST2 => 
-            Q <= "0010"
-            RCO <= '1'
-            if (reset = '1') then nextState <= ST0;
-            elsif (load = '1') then nextState <= loadState;
-            elsif ((enable = '0') and (RCI = '0')) then nextState <= ST3;
-            else nextState <= ST2;
-            end if;
-        when ST3 => 
-            Q <= "0011"
-            RCO <= '1'
-            if (reset = '1') then nextState <= ST0;
-            elsif (load = '1') then nextState <= loadState;
-            elsif ((enable = '0') and (RCI = '0')) then nextState <= ST4;
-            else nextState <= ST3;
-            end if;
-        when ST4 => 
-            Q <= "0100"
-            RCO <= '1'
-            if (reset = '1') then nextState <= ST0;
-            elsif (load = '1') then nextState <= loadState;
-            elsif ((enable = '0') and (RCI = '0')) then nextState <= ST5;
-            else nextState <= ST4;
-            end if;
-        when ST5 => 
-            Q <= "0101"
-            RCO <= '1'
-            if (reset = '1') then nextState <= ST0;
-            elsif (load = '1') then nextState <= loadState;
-            elsif ((enable = '0') and (RCI = '0')) then nextState <= ST6;
-            else nextState <= ST5;
-            end if;
-        when ST6 => 
-            Q <= "0110"
-            RCO <= '1'
-            if (reset = '1') then nextState <= ST0;
-            elsif (load = '1') then nextState <= loadState;
-            elsif ((enable = '0') and (RCI = '0')) then nextState <= ST7;
-            else nextState <= ST6;
-            end if;
-        when ST7 => 
-            Q <= "0111"
-            RCO <= '1'
-            if (reset = '1') then nextState <= ST0;
-            elsif (load = '1') then nextState <= loadState;
-            elsif ((enable = '0') and (RCI = '0')) then nextState <= ST8;
-            else nextState <= ST7;
-            end if;
-        when ST8 => 
-            Q <= "1000"
-            RCO <= '1'
-            if (reset = '1') then nextState <= ST0;
-            elsif (load = '1') then nextState <= loadState;
-            elsif ((enable = '0') and (RCI = '0')) then nextState <= ST9;
-            else nextState <= ST8;
-            end if;
-        when ST9 => 
-            Q <= "1001"
-            RCO <= '0'
-            if (reset = '1') then nextState <= ST0;
-            elsif (load = '1') then nextState <= loadState;
-            elsif ((enable = '0') and (RCI = '0')) then nextState <= ST0;
-            else nextState <= ST9;
-            end if;
-        end case;
-    end process comb_proc;
-end arch;
+comb_proc: process (currentState, reset, enable, rci, load, loadState)
+begin
+	case currentState is
+
+	when ST0 =>
+		Q <= "0000";
+		Rco <= '1';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST1;
+		else nextState <= ST0;
+		end if;
+
+when ST1 =>
+		Q <= "0001";
+		Rco <= '1';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST2;
+		else nextState <= ST1;
+		end if;
+
+when ST2 =>
+		Q <= "0010";
+		Rco <= '1';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST3;
+		else nextState <= ST2;
+		end if;
+
+when ST3 =>
+		Q <= "0011";
+		Rco <= '1';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST4;
+		else nextState <= ST3;
+		end if;
+
+when ST4 =>
+		Q <= "0100";
+		Rco <= '1';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST5;
+		else nextState <= ST4;
+		end if;
+
+when ST5 =>
+		Q <= "0101";
+		Rco <= '1';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST6;
+		else nextState <= ST5;
+		end if;
+
+when ST6 =>
+		Q <= "0110";
+		Rco <= '1';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST7;
+		else nextState <= ST6;
+		end if;
+
+when ST7 =>
+		Q <= "0111";
+		Rco <= '1';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST8;
+		else nextState <= ST7;
+		end if;
+
+when ST8 =>
+		Q <= "1000";
+		Rco <= '1';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST9;
+		else nextState <= ST8;
+		end if;
+
+when ST9 =>
+		Q <= "1001";
+		Rco <= '0';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST0;
+		else nextState <= ST9;
+		end if;
+
+when others =>
+		Q <= "0000";
+		Rco <= '1';
+		if (reset = '1') then nextState <= ST0;
+		elsif (load = '1') then nextState <= loadState;
+		elsif ((enable = '0') and (rci = '0')) then nextState <= ST1;
+		else nextState <= ST0;
+		end if;
+
+end case;
+end process comb_proc;
+
+
+end cont10_arch;
